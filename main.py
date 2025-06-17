2fcbe1c6b265efda958479061f387fa52140facd
Ryzentosh <mac_os@iMac-de-Ryzentosh.local>
2025-06-17 16:46:32 -0300

    design do ciclo

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from app import app
+from livereload import Server
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    server = Server(app.wsgi_app)
+    server.serve(debug=True)
+
