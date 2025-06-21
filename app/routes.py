acc1c22d67f58d4e7db7260d0bdfd32e96b4876a
SarafinaNotFound <gabrielap0548@gmail.com>
2025-06-21 16:34:06 -0300

    Adicionando backend, ainda é necessario integração e fazer o ciclo

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app, db
+from datetime import date
 from flask import render_template, request, redirect, url_for
 import sqlite3
 
-from app.models import Usuario
+from app.models import Diario, Usuario
 from app.forms import UsuarioForm, LoginForm
 from flask_login import login_user, logout_user, current_user
 
@@ -11,16 +12,6 @@ def conectar_db():
     conectar = sqlite3.connect()
     return conectar
 
-
-def criar_tabela():
-    conectar = conectar_db()
-    cursor = conectar.cursor()
-    cursor.execute ('''
-                   CREAT TABLE IF NOT EXISTS usuarios (
-                   id INTEGER PRIMARY KEY      AUTOINCREMENT,
-                   nome TEXT NOT NULL,
-                   idade INTEGER)
-                   ''')
 
 @app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
@@ -65,12 +56,89 @@ def meuperfil():
 
 @app.route("/diario")
 def diario():
+    usuario_id_atual = current_user.id 
+    hoje = date.today()
+    
+    entrada_existente = Diario.query.filter_by(
+        usuario_id=usuario_id_atual, 
+        data_criacao=hoje
+    ).first()
+
+    if entrada_existente:
+    #Se tiver registro do banco de dados que ja preencheou o diario hoje, a entrada é negada) e
+    # redireciona para "calendario". colocar uma mensagem depois
+        return redirect(url_for('calendario.html'))
+
+
     return render_template("diario.html")
 
 
 @app.route('/ciclo')
 def ciclo():
     return render_template('ciclo.html')
+
+
+@app.route('/salvar-diario', methods=['POST'])
+#@login_required
+def salvar_diario():
+
+    try:
+        #Usuaria fez o "diario" hoje:
+        dia_de_hoje = Diario(usuario_id=None) #current_user.id, modificar depois
+        
+        #Botões para html
+        todos_os_campos = [
+            # Emoções
+            'feliz', 'triste', 'alteracao_humor', 'sensivel', 'raiva', 'irritavel',
+            'ansiosa', 'falta_controle', 'indiferenca',
+            
+            # Mente
+            'mente_confusa', 'calma', 'estresse', 'motivacao', 'criatividade',
+            'bom_rendimento', 'preguica_desanimo',
+            
+            # Sociabilidade
+            'sociavel', 'introvertida', 'compreensiva', 'amorosa', 'conflituosa',
+            
+            # Lazer
+            'ferias', 'encontro', 'ressaca', 'alcool', 'cigarro',
+            
+            # Sintomas Físicos
+            'dor_cabeca','tensao_corporal', 'dor_corporal', 'insonia', 'queda_cabelo', 'taquicardia',
+            'surto_acne', 'sem_apetite', 'alergia_dermatite', 'gripe', 'alteracao_hormonal', 'problemas_digestivos',
+            
+            # Comportamento do Parceiro
+            'piadas_ofensivas', 'chantagem', 'mentira', 'dar_gelo', 'ciumes',
+            'culpar', 'desqualificar', 'humilhar', 'xingamentos', 'ameacar',
+            'proibir', 'destruir_bens', 'apertar', 'brincar_bater', 'beliscar',
+            'empurrar', 'bater', 'chutar', 'confinar', 'ameacar_objetos',
+            'ameacar_armas', 'ameacar_morte', 'obrigou_relacao_sexual',
+            'abuso_sexual', 'sufocar', 'feriu_animal', 'tentou_se_matar'
+        ]
+        
+        for campo in todos_os_campos:
+            if campo in request.form:
+                setattr(dia_de_hoje, campo, True)
+        
+        #Calcular Pontuação
+        dia_de_hoje.pontuacao_total = dia_de_hoje.calcular_pontuacao()
+        
+        #Salvar pontuação no banco de dados
+        db.session.add(dia_de_hoje)
+        db.session.commit()
+        
+        # Redirecionamento pra pagina do index do site
+        redirect(url_for("index"))
+    
+    except Exception:
+        #tratar erros
+        db.session.rollback()
+        return redirect(url_for('index'))
+
+    with app.app_context():
+        #pra criar as colunas no banco de dados
+        db.create_all()
+        print("Tabelas criadas com sucesso.")
+
 
 @app.route('/calendario')
 def calendario():
